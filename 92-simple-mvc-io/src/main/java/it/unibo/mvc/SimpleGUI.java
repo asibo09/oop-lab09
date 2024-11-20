48677cb9d4cc764e3d25bd5ce5532458f696d28a
PietroSiboni <pietro.siboni3@studio.unibo.it>
2024-11-20 19:54:53 +0100

    implementation of SimpleGui.java and starting implementation of SimpleGuiWithFileChooser.java, only the same part of SimpleGui.java

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,15 @@
 package it.unibo.mvc;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 /**
  * A very simple program using a graphical interface.
@@ -8,6 +17,42 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
-    private final JFrame frame = new JFrame();
+    private final JFrame frame = new JFrame("My first Java graphical interface");
 
+    private SimpleGUI(final Controller controller) {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        final JTextArea textArea = new JTextArea();
+        final JPanel panel = new JPanel();
+        final LayoutManager layout = new BorderLayout();
+        panel.setLayout(layout);
+
+        final JButton save = new JButton("Save");
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                try {
+                    controller.save(textArea.getText());
+                } catch (IOException e1) {
+                    JOptionPane.showMessageDialog(null, e1.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+        panel.add(textArea, BorderLayout.CENTER);
+        panel.add(save, BorderLayout.SOUTH);
+        frame.setContentPane(panel);
+
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / 4, sh / 4);
+        frame.setLocationByPlatform(true);
+    }
+    private void display() {
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        final SimpleGUI gui = new SimpleGUI(new Controller());
+        gui.display();
+    }
 }
--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
@@ -1,6 +1,15 @@
 package it.unibo.mvc;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 
 /**
@@ -9,6 +18,27 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUIWithFileChooser {
 
-    private final JFrame frame = new JFrame();
+    private final JFrame frame = new JFrame("My second Java graphical interface");
+
+    private SimpleGUIWithFileChooser(final Controller controller) {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        final JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        final JTextField textField = new JTextField();
+        final JButton save = new JButton("Save");
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent event) {
+                try {
+                    controller.save(textField.getText());
+                } catch (IOException e) {
+                    JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+        panel.add(save, BorderLayout.SOUTH);
+        panel.add(textField, BorderLayout.CENTER);
+    }
 
 }
