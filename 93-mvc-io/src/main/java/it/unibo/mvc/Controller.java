f06fa0486fd038dd176a2998b785b7b43d19eaad
PietroSiboni <pietro.siboni3@studio.unibo.it>
2024-11-21 10:32:39 +0100

    implemented the two classes of Controller

--- 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,19 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  *
  */
 public interface Controller {
+
+
+    void setNextStringToPrint(String nextString);
+
+    String getNextStringToPrint();
+
+    List<String> getHistoryPrintedStrings();
+
+    void printCurrentString();
 
 }
--- 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,42 @@
 package it.unibo.mvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 
  *
  */
 public final class SimpleController implements Controller {
 
+    private List<String> historyList = new ArrayList<>();
+    private String nextString;
+
+    @Override
+    public void setNextStringToPrint(String nextString) {
+        if (nextString==null) {
+            throw new IllegalStateException("String can't be null");
+        }
+        historyList.add(nextString);
+        this.nextString=nextString;
+    }
+
+    @Override
+    public String getNextStringToPrint() {
+        return this.nextString;
+    }
+
+    @Override
+    public List<String> getHistoryPrintedStrings() {
+        return List.copyOf(historyList);
+    }
+
+    @Override
+    public void printCurrentString() {
+        if (nextString==null) {
+            throw new IllegalStateException("No string is set");
+        }
+        historyList.add(nextString);
+        System.out.println(nextString); // NOPMD: allowed as this is just an exercise
+    }
 }
